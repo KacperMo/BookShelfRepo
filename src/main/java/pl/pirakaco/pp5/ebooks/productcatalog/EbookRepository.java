ee6a68699f77f74eac61a3f118defaa9dac4412c
Piotr Rachwalik <piotr.rachwalik@gmail.com>
2020-01-18 21:25:03 +0100

    modified CRUD and added tests for it

--- src/main/java/pl/pirakaco/pp5/ebooks/productcatalog/EbookRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.pirakaco.pp5.ebooks.productcatalog;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EbookRepository extends CrudRepository<EbookData, String> {
-}
